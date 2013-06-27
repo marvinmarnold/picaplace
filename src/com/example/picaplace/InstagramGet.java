dca3c378395a7d871c08e272111d4ecdd33ef790
Itai Bez <itaibez96@gmail.com>
2013-06-27 18:56:38 +0300

    Working Instagram

--- src/com/example/picaplace/InstagramGet.java
+++ src/com/example/picaplace/InstagramGet.java
@@ -10,9 +10,13 @@ import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.util.Log;
 
 public class InstagramGet extends Activity {
@@ -23,7 +27,7 @@ public class InstagramGet extends Activity {
 	}
 
 	public static String getPictureStream() {
-		
+
 		Log.d("Instagram", "Getting picture started");
 		StringBuilder builder = new StringBuilder("");
 		HttpClient client = new DefaultHttpClient();
@@ -53,4 +57,47 @@ public class InstagramGet extends Activity {
 
 		return builder.toString();
 	}
+
+	public static String getPictureStream(double lat, double lng) {
+
+		Log.d("Instagram", "Getting picture started");
+		StringBuilder builder = new StringBuilder("");
+		HttpClient client = new DefaultHttpClient();
+		HttpGet httpGet = new HttpGet(
+				"https://api.instagram.com/v1/media/search?lat="
+						+ lat
+						+ "&lng="
+						+ lng
+						+ "&distance=1000&client_id=eaab0a729f20412b922747020fb4798a");
+
+		try {
+			HttpResponse response = client.execute(httpGet);
+			StatusLine statusLine = response.getStatusLine();
+			int statusCode = statusLine.getStatusCode();
+			if (statusCode == 200) {
+				HttpEntity entity = response.getEntity();
+				InputStream content = entity.getContent();
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(content));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					builder.append(line);
+				}
+				Log.d("Instagram", builder.toString());
+			} else {
+				Log.e("TEST", "Failed to download file");
+			}
+		} catch (Exception a) {
+			Log.d("Instagram", a.toString());
+		}
+
+		return builder.toString();
+	}
+	/*
+	 * public static void transformStringToArray(String json, String[] values) {
+	 * try { JSONObject jsonObj = new JSONObject(json); jsonObj =
+	 * jsonObj.getJSONObject(""); } catch (Exception e) { e.printStackTrace(); }
+	 * 
+	 * }
+	 */
 }
