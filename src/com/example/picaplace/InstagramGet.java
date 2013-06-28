40e7d5b498807312bd8bafe891fc80e491bab910
Itai Bez <itaibez96@gmail.com>
2013-06-28 12:24:19 +0300

    Itai Commit

--- src/com/example/picaplace/InstagramGet.java
+++ src/com/example/picaplace/InstagramGet.java
@@ -10,6 +10,7 @@ import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -24,7 +25,7 @@ public class InstagramGet extends Activity {
 
 	public static String getPictureStream() {
 
-		Log.d("Instagram", "Getting picture started");
+		Log.d("PicAInstagram", "Getting picture started");
 		StringBuilder builder = new StringBuilder("");
 		HttpClient client = new DefaultHttpClient();
 		HttpGet httpGet = new HttpGet(
@@ -43,12 +44,12 @@ public class InstagramGet extends Activity {
 				while ((line = reader.readLine()) != null) {
 					builder.append(line);
 				}
-				Log.d("Instagram", builder.toString());
+				Log.d("PicAInstagram", builder.toString());
 			} else {
 				Log.e("TEST", "Failed to download file");
 			}
 		} catch (Exception a) {
-			Log.d("Instagram", a.toString());
+			Log.d("PicAInstagram", a.toString());
 		}
 
 		return builder.toString();
@@ -56,7 +57,7 @@ public class InstagramGet extends Activity {
 
 	public static String getPictureStream(double lat, double lng) {
 
-		Log.d("Instagram", "Getting picture started");
+		Log.d("PicAInstagram", "Getting picture started");
 		StringBuilder builder = new StringBuilder("");
 		HttpClient client = new DefaultHttpClient();
 		HttpGet httpGet = new HttpGet(
@@ -79,21 +80,120 @@ public class InstagramGet extends Activity {
 				while ((line = reader.readLine()) != null) {
 					builder.append(line);
 				}
-				Log.d("Instagram", builder.toString());
+				Log.d("PicAInstagram", builder.toString());
 			} else {
 				Log.e("TEST", "Failed to download file");
 			}
 		} catch (Exception a) {
-			Log.d("Instagram", a.toString());
+			Log.d("PicAInstagram", a.toString());
 		}
 
 		return builder.toString();
 	}
-	/*
-	 * public static void transformStringToArray(String json, String[] values) {
-	 * try { JSONObject jsonObj = new JSONObject(json); jsonObj =
-	 * jsonObj.getJSONObject(""); } catch (Exception e) { e.printStackTrace(); }
-	 * 
-	 * }
-	 */
+
+	// This method get all sub JSonObjects in a json string and returns the
+	// value the most inner one.
+	// String
+	public static String parseJsonString(String json, String[] values) {
+		String value = null;
+		try {
+			// Log.d("PicAJSON", json);
+			JSONObject jsonObj = new JSONObject(json);
+			// Log.d("PicAJSON", jsonObj.toString());
+			for (int i = 0; i < values.length - 1; i++) {
+				jsonObj = jsonObj.getJSONObject(values[i]);
+				// Log.d("PicAJSON", jsonObj.toString());
+			}
+			value = jsonObj.getString(values[values.length - 1]);
+			// Log.d("Val", value + "");
+		} catch (Exception e) {
+			e.printStackTrace();
+			// Log.d("PicAJSON", "ParseCrashed");
+
+		}
+		return value;
+	}
+
+	public static int parseJsonInt(String json, String[] values) {
+		int value = 0;
+		try {
+			Log.d("PicAJSON", json);
+			JSONObject jsonObj = new JSONObject(json);
+			Log.d("PicAJSON", jsonObj.toString());
+			for (int i = 0; i < values.length - 1; i++) {
+				jsonObj = jsonObj.getJSONObject(values[i]);
+				Log.d("PicAJSON", jsonObj.toString());
+			}
+			value = jsonObj.getInt(values[values.length - 1]);
+			Log.d("PicAJSON", value + "");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			Log.d("PicAJSON", "ParseCrashed");
+		}
+		return value;
+	}
+
+	public static double parseJsonDouble(String json, String[] values) {
+		double value = 0;
+		try {
+			JSONObject jsonObj = new JSONObject(json);
+			for (int i = 0; i < values.length - 1; i++) {
+				jsonObj = jsonObj.getJSONObject(values[i]);
+			}
+			value = jsonObj.getDouble(values[values.length - 1]);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return value;
+	}
+
+	// Range between 0 to 15 inclusive.
+	public static String getNumberOfLikes(String json, int start, int end) {
+		String value = "";
+		Log.d("PicAJSON", "Likes started");
+		try {
+			JSONObject jsonObject = new JSONObject(json);
+			for (int i = start; i <= (end % 16); i++) {
+				Log.d("PicAJSON", "Likes: " + i);
+				JSONObject cur = jsonObject.getJSONArray("data")
+						.getJSONObject(i).getJSONObject("likes");
+				value += "Likes " + cur.getInt("count") + "\n";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			Log.e("PicAJSON", "Likes Catched");
+		}
+		return value;
+	}
+
+	public static String getImageUrl(String json) {
+		String value = "";
+		Log.d("PicAJSON", "Likes started");
+		try {
+			JSONObject jsonObject = new JSONObject(json);
+			JSONObject cur = jsonObject.getJSONArray("data").getJSONObject(0)
+					.getJSONObject("images").getJSONObject("thumbnail");
+			value = cur.getString("url");
+		} catch (Exception e) {
+			e.printStackTrace();
+			Log.e("PicAJSON", "Likes Catched");
+		}
+		return value;
+	}
+
+	public static int getMetaCode(String json) {
+		int value = 0;
+		Log.d("PicAJSON", "Meta code started");
+		try {
+			JSONObject jsonObject = new JSONObject(json).getJSONObject("meta");
+			return jsonObject.getInt("code");
+		} catch (Exception e) {
+			e.printStackTrace();
+			Log.e("PicAJSON", "Meta Code failed");
+		}
+		Log.d("PicAJSON", "Meta code started");
+		return value;
+	}
+
 }
--- src/com/example/picaplace/MainActivity.java
+++ src/com/example/picaplace/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.picaplace;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
 import android.view.Menu;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -12,17 +20,25 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.instagram);
+
+		Drawable image = ImageOperations(this, path, "ic_launcher-web.png");
+		Log.d("PicAMain", "Image.jpg is ok");
+		imageView = new ImageView(this);
+		imageView = (ImageView) findViewById(R.id.image);
+		imageView.setImageDrawable(image);
+
 		Log.d("MainActivity", "Activity is created");
 		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
 				.permitAll().build();
 		StrictMode.setThreadPolicy(policy);
 		Log.d("MainActivity", "Changed policy");
 		String json = InstagramGet.getPictureStream();
+		// String[] valuesArr = { "data", "images", "thumbnail", "url" };
 		Log.d("Instagram", "Instagram Done");
+		String code = InstagramGet.getNumberOfLikes(json, 0, 15);
 		TextView myTextView = (TextView) findViewById(R.id.main_text);
-		myTextView.setText(json);
-
+		myTextView.setText(code); // Set the window text
 	}
 
 	@Override
@@ -30,6 +46,29 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	private String path = "http://peytonhamil.com/suns.jpg";
+	private ImageView imageView;
+
+	public Object fetch(String address) throws MalformedURLException,
+			IOException {
+		URL url = new URL(address);
+		Object content = url.getContent();
+		return content;
+	}
+
+	private Drawable ImageOperations(Context ctx, String url,
+			String saveFilename) {
+		try {
+			InputStream is = (InputStream) this.fetch(url);
+			Drawable d = Drawable.createFromStream(is, "src");
+			return d;
+		} catch (MalformedURLException e) {
+			return null;
+		} catch (IOException e) {
+			return null;
+		}
 	}
 
 }
