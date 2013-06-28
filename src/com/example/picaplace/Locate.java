6acc79a155d6443621b4bd04479fb96b39c03352
Yaron Benari <yaronz96@gmail.com>
2013-06-28 12:27:42 +0300

    eddited GPS Related classes

--- src/com/example/picaplace/Locate.java
+++ src/com/example/picaplace/Locate.java
@@ -7,106 +7,106 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Locate extends Activity {
-	private LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+public class Locate extends Activity implements LocationListener {
+	private LocationManager manager;
 	private LocationListener locationListener;
 	private static Location location;
+	private static final int TWO_MINUTES = 1000 * 60 * 2;
+	// The minimum distance to change Updates in meters
+	private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	// The minimum time between updates in milliseconds
+	private static final long MIN_TIME_BW_UPDATES = 1000 * 10 * 1; // 1 minute
+	boolean canGetLocation = false;
+	double lat; // latitude
+	double lon; // longitude
+
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.testinggps);
-		// location =
-		// manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		// TextView view = (TextView) findViewById(R.id.Coords);
+		manager = (LocationManager) this
+				.getSystemService(Context.LOCATION_SERVICE);
+		location = getLocation();
+		TextView view = (TextView) findViewById(R.id.Coords);
 		// view.setText(location.getLongitude() + ", " +
 		// location.getLatitude());
+		view.setText(location.getLatitude() + ", " + location.getLongitude());
+		Button b = (Button) findViewById(R.id.refresh);
+		b.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				getLocation();
+			}
+		});
 	}
 
 	// starts the process getting the location of the users
 	public void startLocationUpdates() {
 		location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		locationListener = new LocationListener() {
-			private static final int TWO_MINUTES = 1000 * 60 * 2;
-
-			public void onLocationChanged(Location location) {
-				makeUseOfNewLocation(location);
-			}
-
-			private void makeUseOfNewLocation(Location location) {
-				if (isBetterLocation(location, Locate.location))
-					Locate.setLocation(location);
-				Log.d("Test", "Location set");
-			}
-
-			private boolean isBetterLocation(Location location,
-					Location bestLocation) {
-				// Check whether the new location fix is newer or older
-				long timeDelta = location.getTime() - bestLocation.getTime();
-				boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
-				boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
-				boolean isNewer = timeDelta > 0;
-				// If it's been more than two minutes since the current
-				// location, use the new location
-				// because the user has likely moved
-				if (isSignificantlyNewer) {
-					return true;
-					// If the new location is more than two minutes older, it
-					// must be worse
-				} else if (isSignificantlyOlder) {
-					return false;
-				}
-				// Check if the old and new location are from the same provider
-				boolean isFromSameProvider = isSameProvider(
-						location.getProvider(), bestLocation.getProvider());
-				// Check whether the new location fix is more or less accurate
-				int accuracyDelta = (int) (location.getAccuracy() - bestLocation
-						.getAccuracy());
-				boolean isLessAccurate = accuracyDelta > 0;
-				boolean isMoreAccurate = accuracyDelta < 0;
-				boolean isSignificantlyLessAccurate = accuracyDelta > 200;
-				// Determine location quality using a combination of timeliness
-				// and accuracy
-				if (isMoreAccurate) {
-					return true;
-				} else if (isNewer && !isLessAccurate) {
-					return true;
-				} else if (isNewer && !isSignificantlyLessAccurate
-						&& isFromSameProvider) {
-					return true;
-				}
-				return false;
-			}
-
-			private boolean isSameProvider(String provider1, String provider2) {
-				if (provider1 == null) {
-					return provider2 == null;
-				}
-				return provider1.equals(provider2);
-			}
-
-			public void onStatusChanged(String provider, int status,
-					Bundle extras) {
-			}
-
-			public void onProviderEnabled(String provider) {
-			}
-
-			public void onProviderDisabled(String provider) {
-			}
-		};
-		// when the location of the user changes it starts the process of
-		// listening to detect the new location and to make use of it to provide
-		// the requested data
 		manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
 				locationListener);
 	}
 
 	// start getting location from GPS providers to detect the desired location
 
-	public void stopListening() {
+	public void stopLocationUpdates() {
 		manager.removeUpdates(locationListener);
+	}
+
+	public Location getLocation() {
+		try {
+			manager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+
+			// getting GPS status
+			boolean isGPSEnabled = manager
+					.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+			// getting network status
+			boolean isNetworkEnabled = manager
+					.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+			if (!isGPSEnabled && !isNetworkEnabled) {
+				// no network provider is enabled
+			} else {
+				this.canGetLocation = true;
+				// if GPS Enabled get lat/long using GPS Services
+				if (isGPSEnabled) {
+					if (location == null) {
+						manager.requestLocationUpdates(
+								LocationManager.GPS_PROVIDER,
+								MIN_TIME_BW_UPDATES,
+								MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+						Log.d("GPS", "GPS Enabled");
+					}
+					if (manager != null) {
+						location = manager
+								.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+						Log.d("GPS", "Got Position");
+						if (location == null)
+							Log.d("GPS", "location null");
+						if (location != null) {
+							Log.d("GPS", "Started change");
+							lat = location.getLatitude();
+							lon = location.getLongitude();
+							Log.d("GPS", "Finished change");
+							Log.d("GPS", "Changed GPS Coords");
+						}
+						Log.d("GPS", "Checking");
+					}
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return location;
 	}
 
 	// stops detecting location of the user after they finish navigating on the
@@ -134,8 +134,32 @@ public class Locate extends Activity {
 		this.locationListener = locationListener;
 	}
 
-	public static Location getLocation() {
+	public static Location getLocation1() {
 		return location;
+	}
+
+	@Override
+	public void onLocationChanged(Location arg0) {
+		// TODO Auto-generated method stub
+		Log.d("GPS", "Changed GPS Location");
+	}
+
+	@Override
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderEnabled(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
