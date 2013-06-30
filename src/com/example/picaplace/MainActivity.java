9362af6a7555b514616930b1f73d205cf66505fb
Itai Bez <itaibez96@gmail.com>
2013-06-30 10:25:39 +0300

    Pic by URL working

--- src/com/example/picaplace/MainActivity.java
+++ src/com/example/picaplace/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.picaplace;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import android.app.Activity;
-import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.StrictMode;
@@ -21,19 +18,23 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.instagram);
-
-		Drawable image = ImageOperations(this, path, "ic_launcher-web.png");
-		Log.d("PicAMain", "Image.jpg is ok");
-		imageView = new ImageView(this);
-		imageView = (ImageView) findViewById(R.id.image);
-		imageView.setImageDrawable(image);
-
 		Log.d("MainActivity", "Activity is created");
 		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
 				.permitAll().build();
 		StrictMode.setThreadPolicy(policy);
 		Log.d("MainActivity", "Changed policy");
 		String json = InstagramGet.getPictureStream();
+
+		try {
+			ImageView myImageView = (ImageView) findViewById(R.id.image);
+			URL thumb_u = new URL(InstagramGet.getImageUrl(json));
+			Drawable thumb_d = Drawable.createFromStream(thumb_u.openStream(),
+					"src");
+			myImageView.setImageDrawable(thumb_d);
+		} catch (Exception e) {
+			// handle it
+		}
+
 		// String[] valuesArr = { "data", "images", "thumbnail", "url" };
 		Log.d("Instagram", "Instagram Done");
 		String code = InstagramGet.getNumberOfLikes(json, 0, 15);
@@ -48,27 +49,18 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
-	private String path = "http://peytonhamil.com/suns.jpg";
-	private ImageView imageView;
+	// Image setting
 
-	public Object fetch(String address) throws MalformedURLException,
-			IOException {
-		URL url = new URL(address);
-		Object content = url.getContent();
-		return content;
-	}
+	private Drawable LoadImageFromWebOperations(String url) {
 
-	private Drawable ImageOperations(Context ctx, String url,
-			String saveFilename) {
 		try {
-			InputStream is = (InputStream) this.fetch(url);
-			Drawable d = Drawable.createFromStream(is, "src");
+			InputStream is = (InputStream) new URL(url).getContent();
+			Drawable d = Drawable.createFromStream(is, "src name");
 			return d;
-		} catch (MalformedURLException e) {
-			return null;
-		} catch (IOException e) {
+		} catch (Exception e) {
+			System.out.println("Exc=" + e);
 			return null;
 		}
-	}
 
+	}
 }
