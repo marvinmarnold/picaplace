1c43d6f989431d5633a87a0c1eb086c99cee577a
Amin <ameennasser@rfs.edu.ps>
2013-07-04 18:47:25 +0300

    ameen design

---
+++ src/com/example/picaplace/LocateDummy.java
@@ -0,0 +1,12 @@
+package com.example.picaplace;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LocateDummy extends Activity {
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home_page_n7);
+	}
+}
